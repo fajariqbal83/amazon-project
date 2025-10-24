dc8695512bd2512234a8a50fd1a01503eea1402f
fajariqbal83 <nazirshazia177@gmail.com>
2025-10-24 15:25:37 +0500

    add

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -38,7 +38,7 @@ let cartSummaryHTML = '';
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary">
+                  <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
                     Delete
                   </span>
                 </div>
@@ -95,3 +95,11 @@ let cartSummaryHTML = '';
 });
 
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
+
+document.querySelectorAll('.js-delete-link').forEach((link) => {
+  link.addEventListener('click', () => {
+   const productId = link.dataset.productId;
+   console.log(productId);
+  })
+
+});
