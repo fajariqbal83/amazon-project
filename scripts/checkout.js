748c5681485d75a21158b1ecdf32208f34cbd252
fajariqbal83 <nazirshazia177@gmail.com>
2025-10-25 12:49:39 +0500

    add modules

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart } from "../data/cart.js";
+import { cart, removeFromCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utiles/money.js";
 
@@ -15,7 +15,7 @@ let cartSummaryHTML = '';
   });
   
   cartSummaryHTML +=`
-      <div class="cart-item-container">
+      <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -99,7 +99,12 @@ document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
 document.querySelectorAll('.js-delete-link').forEach((link) => {
   link.addEventListener('click', () => {
    const productId = link.dataset.productId;
-   console.log(productId);
-  })
+   removeFromCart(productId);
+
+  const container = document.querySelector(
+    `.js-cart-item-container-${productId}`
+   );
+  container.remove();
+  });
 
 });
