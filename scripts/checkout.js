db6a268d846684fe60af49c48da74c35d155a6d0
fajariqbal83 <nazirshazia177@gmail.com>
2025-11-10 12:36:45 +0500

    oop

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -4,6 +4,8 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 
 import { renderPaymentSummary } from  "./checkout/paymentSummary.js";
 
+import '../data/cart-oop.js'
+
 renderOrderSummary();
  
 renderPaymentSummary();
